36e74615c42f1df81e8f383344f9a2c7d0c4e311
Pranav Bhatnagar <unknownblueguy6>
2018-08-23 22:51:14 +0530

    Add more fixes to code

--- conway.py
+++ conway.py
@@ -2,8 +2,8 @@ import pygame, sys, platform
 from pygame.locals import *
 
 CELL_SIZE = 10
-CELLS_PER_ROW = 60
-CELLS_PER_COL = 60
+CELLS_PER_ROW = 50
+CELLS_PER_COL = 50
 WINDOWWIDTH = CELLS_PER_COL*CELL_SIZE
 WINDOWHEIGHT = CELLS_PER_ROW*CELL_SIZE + 2*CELL_SIZE
 
@@ -17,7 +17,7 @@ class Cell:
 	size = CELL_SIZE
 
 	def __init__(self, x, y):
-		self.x = x * self.size;
+		self.x = x * self.size
 		self.y = y * self.size
 		self.rect = pygame.Rect(self.x, self.y, self.size, self.size)
 		self.state = DEAD
@@ -50,30 +50,51 @@ def init():
 		for j in range(CELLS_PER_COL):
 			grid[i].append(Cell(i, j))
 
+
+def convertCoord(x, y):
+	return (CELLS_PER_ROW/2, CELLS_PER_COL/2 - y)
+
+
 def update():
-	for i in range(1, len(grid)-1):
-		for j in range(1, len(grid[i])-1):
-			if (grid[i][j].state == ALIVE):
-				alive = 0
-				for k in range(i-1, i+2):
-					for l in range(j-1, j+2):
-						if k == l : 
-							continue
-						if grid[j][k].state == ALIVE:
-							alive += 1
+	global grid
+	gridCopy = grid
+	
+	for i in range(len(gridCopy)):
+		for j in range(len(gridCopy[i])):
+			if i == 10 and j == 10:
+				print(gridCopy[i][j].state)
+
+			alive = 0
+			
+			for k in range(i-1, i+2):
+				if k == -1:
+					k+=1
+				if k == len(gridCopy):
+					k-=1
+					
+				for l in range(j-1, j+2):
+					if l == -1:
+						l+=1
+					if l == len(gridCopy[i]):
+						l-=1
+					if k == l : 
+						continue
+					if grid[k][l].state == ALIVE:
+						alive += 1
+			
+			if grid[i][j].state == ALIVE:
 				if (alive != 2) or (alive != 3):
-					grid[i][j].deactivate()
+					gridCopy[i][j].deactivate()
+			
 			else:
-				alive = 0
-				for k in range(i-1, i + 2):
-					for l in range(j-1, j+2):
-						if k == l : 
-							continue
-						if grid[j][k].state == ALIVE:
-							alive += 1
 				if alive == 3:
-					grid[i][j].activate()
-			grid[i][j].draw()
+					gridCopy[i][j].activate()
+	
+	grid = gridCopy
+
+	for row in grid:
+		for cell in row:
+			cell.draw()
 
 def exit():
 	pygame.quit()
@@ -81,15 +102,25 @@ def exit():
 
 init()
 
-grid[30][30].activate()
-grid[30][32].activate()
-grid[31][32].activate()
-grid[31][31].activate()
-grid[32][31].activate()
+grid[10][10].activate()
+grid[11][11].activate()
+grid[12][10].activate()
+grid[12][11].activate()
+grid[11][12].activate()	
+
+
+# for row in grid:
+# 	for cell in row:
+# 		cell.draw()
+# pygame.display.update()
+# mainClock.tick(1)
 
 while True:
 	update()
 	pygame.display.update()
+	for cell in grid[30]:
+		if (cell.state):
+			print('oh yeah')
 	mainClock.tick(1)
 
 exit()
