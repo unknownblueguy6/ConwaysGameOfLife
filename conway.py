9672bc62d3e61299e29a128599e3fe1cf31494ae
Pranav Bhatnagar <unknownblueguy6>
2018-08-24 19:30:44 +0530

    Speed up the game

--- conway.py
+++ conway.py
@@ -1,4 +1,5 @@
 import pygame, sys, copy, platform
+from timeit import default_timer as timer
 from pygame.locals import *
 
 CELL_SIZE = 9
@@ -17,27 +18,6 @@ DEAD_COLOUR = WHITE = (255, 255, 255)
 ALIVE_COLOUR = BLACK = (0, 0, 0)
 GREY = (226, 230, 222)
 
-class Cell:
-	size = CELL_SIZE
-
-	def __init__(self, x, y):
-		self.x = x 
-		self.y = y
-		self.state = DEAD
-		self.colour = DEAD_COLOUR
-
-	def activate(self):
-		self.state = ALIVE
-		self.colour = ALIVE_COLOUR
-
-	def deactivate(self):
-		self.state = DEAD
-		self.colour = DEAD_COLOUR
-
-	def draw(self):
-		pygame.draw.rect(windowSurface, self.colour, pygame.Rect((self.x+int(CELLS_PER_ROW/2))* self.size, (self.y+int(CELLS_PER_COL/2)) * self.size, self.size, self.size))
-		pygame.draw.rect(windowSurface, GREY, pygame.Rect((self.x+int(CELLS_PER_ROW/2))* self.size, (self.y+int(CELLS_PER_COL/2)) * self.size, self.size, self.size), 1)
-
 grid = []
 
 def init():
@@ -45,60 +25,76 @@ def init():
 	global windowSurface
 	global mainClock
 	mainClock = pygame.time.Clock()
-	windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
+	windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), DOUBLEBUF)
 	windowSurface.fill(WHITE)
 	pygame.display.set_caption("Conway's Game of Life")
 
 	for i in range(MAX_CELLS_PER_ROW):
 		grid.append([])
 		for j in range(MAX_CELLS_PER_COL):
-			grid[i].append(Cell(i-int(MAX_CELLS_PER_ROW/2), j-int(MAX_CELLS_PER_COL/2)))
+			grid[i].append(False)
 
 
 def getCell(x, y):
 	global grid
 	return grid[x+int(MAX_CELLS_PER_ROW/2)][y+int(MAX_CELLS_PER_COL/2)]
 
+def changeState(x, y):
+	global grid
+	grid[x+int(MAX_CELLS_PER_ROW/2)][y+int(MAX_CELLS_PER_COL/2)] = not(grid[x+int(MAX_CELLS_PER_ROW/2)][y+int(MAX_CELLS_PER_COL/2)])
+
+def drawCell(x, y):
+	if(getCell(x,y) == ALIVE):
+		colour = ALIVE_COLOUR
+	else:
+		colour = DEAD_COLOUR
+	pygame.draw.rect(windowSurface, colour, pygame.Rect((x+int(CELLS_PER_ROW/2))* CELL_SIZE, (y+int(CELLS_PER_COL/2)) * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+	pygame.draw.rect(windowSurface, GREY, pygame.Rect((x+int(CELLS_PER_ROW/2))* CELL_SIZE, (y+int(CELLS_PER_COL/2)) * CELL_SIZE, CELL_SIZE, CELL_SIZE), 1)
+
 def getX():
 	global grid
 	x1 = MAX_CELLS_PER_ROW
 	x2 = -MAX_CELLS_PER_ROW
-	for row in grid:
-		for cell in row:
-			if cell.state == ALIVE :
-				if cell.x < x1:
-					x1 = cell.x
-				if cell.x > x2:
-					x2 = cell.x
+	for i in range(-int(MAX_CELLS_PER_ROW/2), int(MAX_CELLS_PER_ROW/2) + 1):
+		for j in range(-int(MAX_CELLS_PER_COL/2), int(MAX_CELLS_PER_COL/2) + 1):
+			if getCell(i, j) == ALIVE :
+				if i < x1:
+					x1 = i
+				if i > x2:
+					x2 = i
 	return x1 - 1, x2 + 2
 
 def getY():
 	global grid
 	y1 = MAX_CELLS_PER_COL
 	y2 = -MAX_CELLS_PER_COL
-	for row in grid:
-		for cell in row:
-			if cell.state == ALIVE :
-				if cell.y < y1:
-					y1 = cell.y
-				if cell.y > y2:
-					y2 = cell.y
+	for i in range(-int(MAX_CELLS_PER_ROW/2), int(MAX_CELLS_PER_ROW/2) + 1):
+		for j in range(-int(MAX_CELLS_PER_COL/2), int(MAX_CELLS_PER_COL/2) + 1):
+			if getCell(i, j) == ALIVE :
+				if j < y1:
+					y1 = j
+				if j > y2:
+					y2 = j
 	return y1 - 1, y2 + 2
 
 def update():
+	
 	global grid
 	
 	for i in range(-int(CELLS_PER_ROW/2), int(CELLS_PER_ROW/2) + 1):
 		for j in range(-int(CELLS_PER_COL/2), int(CELLS_PER_COL/2) + 1):
-			getCell(i, j).draw()
+			drawCell(i, j)
 	
 	gridCopy = copy.deepcopy(grid)
 
+	start = timer()
 	x1, x2 = getX()
 	y1, y2 = getY()
+	end = timer()
 
-	print("%s %s" % (x1, x2))
-	print("%s %s" % (y1, y2))
+	print(end - start)
+	
+	#print("%s,%s %s,%s" % (x1,y1, x2,y2))
 	
 	for i in range(x1, x2):
 		for j in range(y1, y2):
@@ -113,15 +109,15 @@ def update():
 					if (l == -1 + -int(MAX_CELLS_PER_COL/2)) or (l == int(MAX_CELLS_PER_COL/2) + 1) or (k == i and l == j): 
 						continue
 
-					if getCell(k, l).state == ALIVE:
+					if getCell(k, l) == ALIVE:
 						alive += 1
 
-			if getCell(i, j).state == ALIVE:
+			if getCell(i, j) == ALIVE:
 				if not((alive == 2) or (alive == 3)):
-					gridCopy[i + int(MAX_CELLS_PER_ROW/2)][j + int(MAX_CELLS_PER_COL/2)].deactivate()
+					gridCopy[i + int(MAX_CELLS_PER_ROW/2)][j + int(MAX_CELLS_PER_COL/2)] = False
 			else:
 				if alive == 3:
-					gridCopy[i + int(MAX_CELLS_PER_ROW/2)][j + int(MAX_CELLS_PER_COL/2)].activate()
+					gridCopy[i + int(MAX_CELLS_PER_ROW/2)][j + int(MAX_CELLS_PER_COL/2)] = True
 
 	grid = copy.deepcopy(gridCopy)
 
@@ -132,15 +128,15 @@ def exit():
 init()
 
 #glider, just for testing
-# grid[10][8].activate()
-# grid[11][9].activate()
-getCell(0,0).activate()
-getCell(1,1).activate()
-getCell(2,0).activate()
-getCell(2,1).activate()
-getCell(1,2).activate()	
+# grid[10][8] = True
+# grid[11][9] = True
+changeState(0,0)
+changeState(1,1)
+changeState(2,0)
+changeState(2,1)
+changeState(1,2)
 
-# getCell(0, 0).activate()
+# getCell(0, 0) = True
 
 # for i in range(-int(CELLS_PER_ROW/2), int(CELLS_PER_ROW/2) + 1):
 # 	for j in range(-int(CELLS_PER_COL/2), int(CELLS_PER_COL/2) + 1):
