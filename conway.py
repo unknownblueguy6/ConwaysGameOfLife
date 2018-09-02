be4f9ec33ae91ade0a84862e0c9a1025dc37ad9b
Pranav Bhatnagar <unknownblueguy6>
2018-09-03 01:59:34 +0530

    Some fixes to movement

--- conway.py
+++ conway.py
@@ -6,12 +6,12 @@ CELL_SIZE = 12
 CELLS_PER_ROW = 50+1
 CELLS_PER_COL = 50+1
 WINDOWWIDTH = CELLS_PER_COL*CELL_SIZE
-WINDOWHEIGHT = CELLS_PER_ROW*CELL_SIZE + 2*CELL_SIZE
+WINDOWHEIGHT = CELLS_PER_ROW*CELL_SIZE +  2*CELL_SIZE
 
 FPS = 60
 
-MAX_CELLS_PER_ROW = 1000 + 1
-MAX_CELLS_PER_COL = 1000 + 1
+# MAX_CELLS_PER_ROW = 1000 + 1
+# MAX_CELLS_PER_COL = 1000 + 1
 DEAD = False
 ALIVE = True
 
@@ -59,23 +59,48 @@ def drawGrid():
 
 def edit():
 	global gameState
+	
 	x, y = 0, 0
-	while True:
-		for event in pygame.event.get():
-			if event.type == KEYDOWN:
-				if event.key == K_LEFT and x != -CELLS_PER_ROW//2:
-					x -= 1
-				elif event.key == K_RIGHT and x != CELLS_PER_ROW//2:
-					x += 1
-				elif event.key == K_DOWN and y != CELLS_PER_COL//2:
-					y += 1
-				elif event.key == K_UP and y != -CELLS_PER_COL//2: 
-					y -= 1
-				elif event.key == K_s:
-					changeState(x, y)
-				elif event.key == K_SPACE or event.key == K_RETURN:
-					gameState = RUN
-					return
+
+	getTicksLastFrame = pygame.time.get_ticks()
+
+	while gameState == EDIT:
+
+		t = pygame.time.get_ticks()	
+		dt = t - getTicksLastFrame
+		getTicksLastFrame = t
+		
+		pygame.event.pump()
+		
+		keys = pygame.key.get_pressed()
+		
+		if keys[K_RIGHT]:
+			x += 1
+		
+		if keys[K_LEFT]:
+			x -= 1
+		
+		if keys[K_UP]:
+			# y_time = 100
+			y -= 1
+		if keys[K_DOWN]:
+			# y_time = 100
+			y += 1
+		if keys[K_s]:
+			changeState(x, y)
+		
+		if keys[K_SPACE] or keys[K_RETURN]:
+			gameState = RUN
+		
+		if x <= -CELLS_PER_ROW//2:
+			x = -CELLS_PER_ROW//2 + 1
+		elif x >= +CELLS_PER_ROW//2:
+			x = CELLS_PER_ROW//2
+		if y <= -CELLS_PER_COL//2:
+			y = -CELLS_PER_COL//2 + 1
+		elif y >= CELLS_PER_COL//2:
+			y = CELLS_PER_COL//2
+
 		drawGrid()
 		pygame.draw.rect(windowSurface, BLUE, pygame.Rect((x + CELLS_PER_ROW//2)* CELL_SIZE, (y + CELLS_PER_COL//2) * CELL_SIZE, CELL_SIZE, CELL_SIZE), 2)
 		pygame.display.update()
@@ -92,12 +117,12 @@ def update():
 	for x, y in liveCells:
 
 		for i in range(x -1, x + 2):
-			if i == -1 + -MAX_CELLS_PER_ROW//2 or i == MAX_CELLS_PER_ROW//2 + 1:
-				continue	
+			# if i == -1 + -MAX_CELLS_PER_ROW//2 or i == MAX_CELLS_PER_ROW//2 + 1:
+				# continue	
 			
 			for j in range(y -1 , y +2):
-				if (j == -1 + -MAX_CELLS_PER_COL//2) or (j == MAX_CELLS_PER_COL//2 + 1):
-					continue
+				# if (j == -1 + -MAX_CELLS_PER_COL//2) or (j == MAX_CELLS_PER_COL//2 + 1):
+					# continue
 			
 				if (i, j) in checkedCells:
 					continue
@@ -105,11 +130,12 @@ def update():
 				alive = 0
 			
 				for k in range(i-1, i+2):
-					if k == -1 + -MAX_CELLS_PER_ROW//2 or k == MAX_CELLS_PER_ROW//2 + 1:
-						continue
+					# if k == -1 + -MAX_CELLS_PER_ROW//2 or k == MAX_CELLS_PER_ROW//2 + 1:
+						# continue
 					
 					for l in range(j-1, j+2):
-						if (l == -1 + -MAX_CELLS_PER_COL//2) or (l == MAX_CELLS_PER_COL//2 + 1) or (k == i and l == j): 
+						# if (l == -1 + -MAX_CELLS_PER_COL//2) or (l == MAX_CELLS_PER_COL//2 + 1) or
+						if(k == i and l == j): 	
 							continue
 
 						if (k, l) in liveCells:
@@ -128,19 +154,18 @@ def update():
 	# end = timer()
 
 	#print(end - start)
+
 def exit():
 	pygame.quit()
 	sys.exit()
 
-init()
 
+init()
 while True:
 	if(gameState == EDIT):
 		edit()
-	elif(gameState == RUN):
+	if(gameState == RUN):
 		update()
 	pygame.display.update()
 	mainClock.tick(FPS)
-	print(mainClock.get_fps())
-
 exit()
